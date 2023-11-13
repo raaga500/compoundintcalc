8b70ec27caa574f7a8d856c8ee2e7274a34fbef1
Raaga500 <zebratechnologiesindia@gmail.com>
2023-11-13 14:31:27 -0600

    Added Interest earned

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("<h1 style='text-align: center;'>Compound Interest Calculator</h1>",
 
 
 with st.form("compound_interest_form"):
-    P = st.number_input("**Principal Amount**")
+    P = st.number_input("**Principal Amount($)**")
     r = st.number_input("**Rate**")/100
     n_key = st.selectbox("**How many times per year interest is applied**",("Monthly","Quarterly","Semi-Annually","Annually"))
     t = st.slider("**Length in Years**",min_value=1,max_value=50,value=10,step=1)
@@ -32,6 +32,8 @@ n = n_dict[n_key]
 amount = P*(1 + r/n)**(n*t)
 amount = round(amount,2)
 
+interest_earned = amount - P
+
 #------Create empty result container  at the top of the page which will be populated after all the calcullations
 #Result container
 st.divider()
@@ -42,6 +44,6 @@ if submitted:
     #Show results in result container
     with result_container:
         #Show one liner result summary at the bottom of the page
-        result_summary = f'You will get ${amount} in {t} year/s'
+        result_summary = f'You will get ${amount:,} in {t} year/s, with total interest earned of ${interest_earned:,}'
         st.markdown(f"<h2 style='text-align: center;'>{result_summary}</h1>", unsafe_allow_html=True)
         
